54e8c37b84cfa2902d1fa92c4d71e5543ea14698
Tsilkostas <kostas.gv@gmail.com>
2023-01-22 22:31:04 +0200

    commit

--- face_detection.py
+++ face_detection.py
@@ -3,9 +3,13 @@ detect= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 imp_img= cv2.VideoCapture("elon.jpg")
 
 res, img= imp_img.read()
+
 gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces= detect.detectMultiScale(gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255,255, 0), 2)
 
 cv2.imshow("Elon Image", img)
 
